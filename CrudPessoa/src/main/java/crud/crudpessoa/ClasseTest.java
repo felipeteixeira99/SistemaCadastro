fdfbf308f7b47c5b6afebd185c93ccf157322ec9
Felipe <felipefggw@gmail.com>
2023-05-25 22:57:09 -0300

    Classe criada dentro da branch testing

---
+++ CrudPessoa/src/main/java/crud/crudpessoa/ClasseTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud.crudpessoa;
+
+/**
+ *classe criada para testar a branch testing
+ * @author Felipe
+ */
+public class ClasseTest {
+    
+}
